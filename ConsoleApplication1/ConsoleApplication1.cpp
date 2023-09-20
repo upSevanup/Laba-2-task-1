5a877f8f07ef2ce359cc190bb5fd2441fce52ffc
Sevan <Sevan@INBOOK_X2_GEN11>
2023-09-20 19:15:46 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,22 @@
+﻿// Задание 1
+
+#include <iostream>
+#include <clocale>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "ru");
+
+	double a, b, c, p;
+
+	cout << "Введите стороны а и b: ";
+	cin >> a >> b;
+
+	c = sqrt(a * a + b * b); // вычисление гипотенузы
+	p = a + b + c; // вычисление периметра
+
+	cout << "гипотенуза равна: " << c << endl;
+	cout << "периметр равен: " << p << endl;
+}
